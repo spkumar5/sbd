42e8f81d5c0e5f8deca0090fb74840c4a788a73c
E050_APPS_a_PRAVES2 <praveenkumar.s@daimler.com>
2019-03-30 19:27:02 +0530

    Initial commit

---
+++ src/main/java/com/spk/sbd/SbdApplication.java
@@ -0,0 +1,13 @@
+package com.spk.sbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdApplication.class, args);
+	}
+
+}
